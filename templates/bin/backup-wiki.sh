d673800823ac7cc35662a1b99d0f3776af927f9b
Steffen Uhlig <steffen@familie-uhlig.net>
2017-05-20 23:57:01 +0200

    Enable backup rotation

--- templates/bin/backup-wiki.sh
+++ templates/bin/backup-wiki.sh
@@ -3,7 +3,14 @@
 # Generated from <%= template %>
 
 mkdir -p backup/{daily,weekly,monthly,yearly}
+
+echo Creating daily backup
+echo
 tar --create --exclude-vcs --gzip --file "backup/daily/mediawiki_$(date "+%Y-%m-%d_%H%M").tar.gz" mediawiki
+
+echo Rotating backup files
+echo
+bin/rotate-backups
 
 # Report
 echo Available database snapshots:
