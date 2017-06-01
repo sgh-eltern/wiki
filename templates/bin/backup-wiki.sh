5d64b1c76bb4bbb41f4c8895c332402cf414cd28
Steffen Uhlig <steffen@familie-uhlig.net>
2017-06-02 00:00:48 +0200

    Dump the latest database backup and rotate it

--- templates/bin/backup-wiki.sh
+++ templates/bin/backup-wiki.sh
@@ -2,22 +2,56 @@
 
 # Generated from <%= template %>
 
-mkdir -p backup/{daily,weekly,monthly,yearly}
+## mediawiki files
 
-echo Creating daily backup
-echo
-tar --create --exclude-vcs --gzip --file "backup/daily/mediawiki_$(date "+%Y-%m-%d_%H%M").tar.gz" mediawiki
+ROOT=backup/mediawiki-files
+mkdir -p "${ROOT}"/{daily,weekly,monthly,yearly}
 
-echo Rotating backup files
-echo
-bin/rotate-backups
+MEDIAWIKI_BACKUP_FILE="${ROOT}"/daily/mediawiki_$(date "+%Y-%m-%d_%H%M").tar.gz
 
-# Report
-echo Available database snapshots:
 echo
-mysqlbackups "<%= config['source_db'] %>"
+echo "Creating daily backup as ${MEDIAWIKI_BACKUP_FILE} ..."
+tar --create --exclude-vcs --gzip --file "${MEDIAWIKI_BACKUP_FILE}" mediawiki
+echo ...done.
+
 echo
-echo Content of backup directory:
+echo Rotating backup files...
+bin/rotate-backups "${ROOT}"
+echo ...done.
+
 echo
-ls -ltr backup/*
+echo "Contents of backup directory ${ROOT}:"
+echo
+ls -ltr "${ROOT}"/*
+echo
+
+## Database
+
+ROOT=backup/mediawiki-db
+mkdir -p "${ROOT}"/{daily,weekly,monthly,yearly}
+
+DB_BACKUP_LATEST=$(mysqlbackups "<%= config['source_db'] %>" | head -1)
+MEDIAWIKI_DB_BACKUP_FILE="${ROOT}"/daily/mediawiki-db_$(date "+%Y-%m-%d_%H%M").sql.gz
+
+echo
+echo "Saving latest database snapshot ${DB_BACKUP_LATEST} to ${MEDIAWIKI_DB_BACKUP_FILE}:"
+mysqldump \
+    --add-drop-table \
+    --host="${DB_BACKUP_LATEST}" \
+    --user="<%= config['source_user'] %>" \
+    --password="<%= config['source_password'] %>" \
+  "<%= config['source_db'] %>" \
+| gzip \
+> "${MEDIAWIKI_DB_BACKUP_FILE}"
+echo ...done.
+
+echo
+echo Rotating backup files...
+bin/rotate-backups "${ROOT}"
+echo ...done.
+
+echo
+echo "Contents of backup directory ${ROOT}:"
+echo
+ls -ltr "${ROOT}"/*
 echo
