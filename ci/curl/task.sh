b753d0b4695613a8f911624852591bcd90335478
Steffen Uhlig <steffen@familie-uhlig.net>
2017-08-16 20:42:09 +0200

    Add pipeline to trigger Ghost

--- ci/curl/task.sh
+++ ci/curl/task.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 set -e
+curl -v "$url" > result/response
 
-curl "$url" 2> result/log > result/response
+echo
+echo "Result:"
+< result/response jq -r .code | tee result/code
+
+echo
+echo "Screenshot:"
+< result/response jq -r .data[].screenshot.original.defaultUrl | tee result/screenshot
+
+echo
+echo "Video:"
+< result/response jq -r .data[].video.url | tee result/video
