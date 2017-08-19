88ed631fb3d43da8f16c331d84abcb66421d6ac1
Steffen Uhlig <steffen@familie-uhlig.net>
2017-08-19 22:51:58 +0200

    Rename task

--- ci/fetch-result/task.sh
+++ ci/fetch-result/task.sh
@@ -5,7 +5,15 @@ curl -v "$url" > result/response
 
 echo
 echo "Result:"
+< result/response jq .
+
+echo
+echo "Code:"
 < result/response jq -r .code | tee result/code
+
+echo
+echo "Passing:"
+< result/response jq -r .data[0].passing | tee result/passing
 
 echo
 echo "Screenshot:"
