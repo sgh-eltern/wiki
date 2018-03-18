533207d229d3b2c59b8cc92a186f9e20057d3ab1
Steffen Uhlig <steffen@familie-uhlig.net>
2018-03-18 13:53:45 +0100

    Add backup pipelines for Eltern, Freunde and GEB

--- ci/store-backup/task.sh
+++ ci/store-backup/task.sh
@@ -8,7 +8,7 @@ find backup -type f
 # https://www.backblaze.com/b2/docs/quick_command_line.html
 b2 authorize_account "$access_key_id" "$secret_access_key"
 
-# TODO [--delete] [--keepDays N] [--skipNewer] [--replaceNewer] \
+# TODO [--delete] [--keepDays N] [--skipNewer] [--replaceNewer]
 b2 sync backup b2:"$bucket"
 
 echo
